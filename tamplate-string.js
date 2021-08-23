2f2f36b8a5abd66e9634523d1dcf8d9592535b84
Aal Emraan <developer.aalemraan@gmail.com>
2021-08-23 23:01:50 +0600

    second

--- tamplate-string.js
+++ tamplate-string.js
@@ -3,4 +3,12 @@ const kobita = `this is first line
 this is second line
 this is third line
           this is fourth line`;
-console.log(kobita)
+console.log(kobita);
+
+// this backtick can handle any function, variables and all valid javascript within it....
+
+const friends = 5;
+const new1 = `<h3>this is new friend number - ${friends} and his name is ${name}`;
+console.log(new1);
+const about = `his name is ${name}. he read in honours. he has the total amount of money ${friends * 100 + 5000}.`;
+console.log(about);
